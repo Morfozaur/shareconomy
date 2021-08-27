5fc29730abba55792cc5e1bec877965c7f2c7622
Morfozaur <e.borzechowski@gmail.com>
2021-08-27 18:59:42 +0200

    Stylowanie z RWD home-about

--- src/components/home/HomeAbout.js
+++ src/components/home/HomeAbout.js
@@ -7,13 +7,17 @@ const HomeAbout = () => {
     return (
         <section className='about'>
             <div className="about__frame">
-                <div className="about__header">O nas</div>
-                <Decoration/>
-                <p className="about__text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <img src={signature} alt="signature" className="about__signature"/>
+                <div className="about__content">
+                    <div className="about__title">O nas</div>
+                    <Decoration/>
+                    <p className="about__text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                    <img src={signature} alt="signature" className="about__signature"/>
 
+                </div>
             </div>
-            <img src={people} alt="people" className="about__img"/>
+            <div className="about__cover">
+                <img src={people} alt="people" className="about__img"/>
+            </div>
         </section>
     );
 }
