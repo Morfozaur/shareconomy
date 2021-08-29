4f2336fb232c2e8bad8c3d12db3b8348b15524bf
Morfozaur <e.borzechowski@gmail.com>
2021-08-29 21:16:34 +0200

    Poprawa działania fetchowania z firebase'a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/main.scss'
 import App from './js/components/App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
 import {applyMiddleware, compose, createStore} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import {allReducers} from "./js/redux/reducers/allReducers";
+
 
 const composeEnhancers = compose(
     applyMiddleware(thunk)
@@ -17,9 +16,7 @@ const store = createStore(allReducers, composeEnhancers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
             <App />
-        </HashRouter>
     </Provider>,
   document.getElementById('root')
 );
--- src/js/components/home/HomeHelpers.js
+++ src/js/components/home/HomeHelpers.js
@@ -1,59 +1,34 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-// import {fetchLists} from "../../redux/actions/fetchLists";
-import {collection, getDocs} from "firebase/firestore";
-import {db} from "../../firebase";
-import {fetchFoundations, fetchFundraisers, fetchOrganisations} from "../../redux/actions/allFetchers";
-import {isLogged} from "../../redux/reducers/isLogged";
+import {loadData} from "../../redux/actions/loadData";
+// import {isLogged} from "../../redux/reducers/isLogged";
 
 
 const HomeHelpers = () => {
 
     const dispatch = useDispatch();
 
-    const fetchLists = async () => {
-        await getDocs(collection(db, 'foundations'))
-            .then(res => {
-                const data = res.docs.map(post => post.data());
-                dispatch(fetchFoundations(data));
-            })
-
-        await getDocs(collection(db, 'fundraisings'))
-            .then(res => {
-                const data = res.docs.map(post => post.data());
-                dispatch(fetchFundraisers(data));
-            })
-
-        await getDocs(collection(db, 'organisations'))
-            .then(res => {
-                const data = res.docs.map(post => post.data());
-                dispatch(fetchOrganisations(data));
-            })
-    }
-
-
-        const login = useSelector(state => state.isLogged)
-        const foundations = useSelector(state => state.foundations)
+    // const login = useSelector(state => state.isLogged)
+    const foundations = useSelector(state => state.foundations)
     const fundraisers = useSelector(state => state.fundraisers)
     const organisations = useSelector(state => state.organisations)
 
     useEffect(()=> {
-        fetchLists()
-    },[])
+        dispatch(loadData())
+    },[dispatch])
 
-    // console.log(foundations)
     return (
         <section className='helpers'>
-            <p onClick={()=>console.log(foundations, fundraisers, organisations)}>HELPERS</p>
-            {/*{foundations.map(el => {*/}
-            {/*    return (*/}
-            {/*        <>*/}
-            {/*        <h2>{el.name}</h2>*/}
-            {/*        <p>{el.goal}</p>*/}
-            {/*        <strong>{el.request}</strong>*/}
-            {/*        </>*/}
-            {/*    )*/}
-            {/*})}*/}
+            <p>HELPERS</p>
+            {foundations.map(({name, goal, request}) => {
+                return (
+                    <div className='helpers__single' key={name}>
+                        <h2>{name}</h2>
+                        <p>{goal}</p>
+                        <strong>{request}</strong>
+                    </div>
+                )
+            })}
             <p>HELPERS</p>
 
             <p>HELPERS</p>
