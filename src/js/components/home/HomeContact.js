748bd2d30ffd3414a487907ca1aeed81720c45ee
Morfozauf <e.borzechowski@gmail.com>
2021-08-31 12:21:18 +0200

    Start kontaktu - struktura

--- src/js/components/home/HomeContact.js
+++ src/js/components/home/HomeContact.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import
+import Decoration from "../elements/Decoration";
 
 const HomeContact = () => {
     return (
-        <section className='contact' id='contact'>
-            CONTACT
+        <section className='contact'>
+            <div className="contact__content">
+                <div className="contact__header">
+                    <div className="contact__title">f</div>
+                    <Decoration/>
+                </div>
+                <form action="" className="contact__form">
+                    <div className="contact__inputs">
+                        <div className="contact__mail">
+                            <label htmlFor=""></label>
+                            <input type="text"/>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </section>
     );
 }
