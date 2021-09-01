82772795bf6bb7d5f96eed6de45084529dc43e14
Morfozaur <e.borzechowski@gmail.com>
2021-09-01 20:25:51 +0200

    Poprawka przycisku w kontakcie - dodanie klasy btn

--- src/js/components/home/HomeContact.js
+++ src/js/components/home/HomeContact.js
@@ -135,7 +135,7 @@ const HomeContact = () => {
                             {wrongMessage && <>Wiadomość musi mieć co najmniej 120 znaków!</>}
                         </p>
                     </div>
-                    <button className="contact__button" onClick={e=>send(e)}>Wyślij</button>
+                    <button className=" btn contact__button" onClick={e=>send(e)}>Wyślij</button>
                 </form>
             </div>
             <div className="contact__footer">
