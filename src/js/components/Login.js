cc3a04052434ca9ba4c2fb6388a121a0adce57d6
Morfozaur <e.borzechowski@gmail.com>
2021-09-02 21:19:19 +0200

    Dodanie strony logowania

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import Decoration from "./elements/Decoration";
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
         <div className='login'>
             <div className="login__header">
-                <h2 className="font__header">Zaloguj się</h2>
+                <h2 className="login__title font__header">Zaloguj się</h2>
                 <Decoration/>
             </div>
             <div className="login__box">
-                <label htmlFor="email"className='login__label'>Email</label>
-                <input type="text" id="email" className='login__input'/>
-                <label htmlFor="password"className='login__label'>Hasło</label>
-                <input type="password" id="password" className='login__input'/>
+                <div className="login__group">
+                    <label htmlFor="email" className='login__label'>Email</label>
+                    <input type="text"
+                           id="email"
+                           autoComplete='none'
+                           className='input login__input'/>
+                </div>
+                <div className="login__group">
+                    <label htmlFor="password" className='login__label'>Hasło</label>
+                    <input type="password"
+                           id="password"
+                           autoComplete='none'
+                           className='input login__input'/>
+                </div>
             </div>
             <div className="login__buttons">
-                <button className="btn">Załóż konto</button>
-                <button className="btn">Zaloguj się</button>
+                <button className="login__btn btn"><Link to="/rejestracja">Załóż konto</Link></button>
+                <button className="login__btn btn btn--frame">Zaloguj się</button>
             </div>
         </div>
     );
