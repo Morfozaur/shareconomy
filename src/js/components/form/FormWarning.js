e0200665a47d882758f1b1c3866b7174ce0828d2
Morfozaur <e.borzechowski@gmail.com>
2021-09-04 23:49:44 +0200

    Formularz - warning z RWD

--- src/js/components/form/FormWarning.js
+++ src/js/components/form/FormWarning.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const FormInfo = () => {
     return (
-        <div>
-            INFO
+        <div className='warning'>
+            <h3 className="warning__title">Ważne!</h3>
+            <p className="warning__info">Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
         </div>
     );
 }
