aa416bef770ee7d79f322c80edadc4ff6b1d9683
Morfozaur <e.borzechowski@gmail.com>
2021-09-06 18:50:28 +0200

    Formularz częsć pierwsza - poprawa stylowania, rozdzielenie przycisków, RWD

--- src/js/components/form/FormBody.js
+++ src/js/components/form/FormBody.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import FormStepA from "./FormStepA";
 import FormStepB from "./FormStepB";
 import FormStepC from "./FormStepC";
 import FormStepD from "./FormStepD";
 import FormSummary from "./FormSummary";
 import FormFinish from "./FormFinish";
+import {fetchForm} from "../../redux/actions/allFetchers";
 
 const FormBody = () => {
-    const currForm = useSelector(state => state.currForm)
 
+    const dispatch = useDispatch();
+    const currForm = useSelector(state => state.currForm);
+
+    const prevForm = (e) => {
+        e.preventDefault();
+        dispatch(fetchForm(currForm-1))
+    };
+
+    const nextForm = (e) => {
+        e.preventDefault();
+        dispatch(fetchForm(currForm+1));
+    };
 
     return (
         <div className='form'>
@@ -19,6 +31,12 @@ const FormBody = () => {
                 {currForm === 4 && <FormStepD/>}
                 {currForm === 5 && <FormSummary/>}
                 {currForm === 6 && <FormFinish/>}
+            {currForm < 6 &&
+            <div className="form__change">
+                {currForm > 1 &&
+                <button className='btn btn--frame form__controls' onClick={e => prevForm(e)}>Wstecz</button>}
+                <button className='btn btn--frame form__controls' onClick={e => nextForm(e)}>Dalej</button>
+            </div>}
         </div>
     );
 }
